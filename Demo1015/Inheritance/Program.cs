a7e116be290bac9386ec97df2f97daa24dffe4a9
Michael Hart <M.HART@students.clark.edu>
2019-10-15 15:20:42 -0700

    Tuesday 10/15 in-class lab

---
+++ Demo1015/Inheritance/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // test method inheritance
+            ChildClass cc = new ChildClass();
+            GrandChildClass gcc = new GrandChildClass();
+
+            Console.WriteLine("calling method from gcc");
+            gcc.BaseMethod();
+            gcc.ChildMethod();
+            gcc.GrandChildMethod();
+            Console.WriteLine();
+
+            Console.WriteLine("calling method from cc");
+            cc.BaseMethod();
+            cc.ChildMethod();
+            //cc.GrandChildMethod();
+            Console.WriteLine();
+
+            // test base class fields
+            BaseClass bc = new BaseClass(123, 455);
+            bc.PrintState();
+            Console.WriteLine();
+
+            // test child print state
+            ChildClass cc2 = new ChildClass(987, 484, 398);
+            cc2.PrintState();
+        }
+    }
+}
